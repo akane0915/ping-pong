f9a66369b9526508bf59318f6734ce5b10619221
Asia Kane <asialkane@gmail.com>
2017-03-31 09:30:42 -0700

    add user interface logic to store user input as variable

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@
 
 //User Interface Logic
 $(document).ready(function(){
+  $("form#ppform").submit(function(event){
+    event.preventDefault();
+    let userInput = $("input#ppinput").val();
 
-
-
+    $("#ppoutput").text(userInput);
+  }); //Submit form close
 }); //Document.ready function close
