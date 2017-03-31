c21579a2a9bfd63e5c25889ecfea582cb44bc7a4
Asia Kane <asialkane@gmail.com>
2017-03-31 10:10:09 -0700

    add business logic to return an array from 1 to the user input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 //Business Logic
 //Check if user input is positive integer. If not, return invalid input
-let posInteger = function(number){
-  if (number > 0){
-    return number;
-  } else {
+var posInteger = function(number){
+  if (number <= 0){
     return "Invalid input.  Please enter an integer greater than 0."
+  } 
+};
+
+//Creates an array from 1 to the user input number.  Each element in array is a number.
+var createArray = function(number) {
+  debugger;
+  var startArray = [];
+  for (var i=1; i<=number; i++){
+    startArray.push(i);
   }
+  return startArray;
 };
 
 //User Interface Logic
@@ -14,6 +22,7 @@ $(document).ready(function(){
     event.preventDefault();
     let userInput = $("input#ppinput").val();
 
+    $("#ppoutput").text(createArray(userInput));
     $("#ppoutput").text(posInteger(userInput));
   }); //Submit form close
 }); //Document.ready function close
