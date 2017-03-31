92702cff9289d712c1fea7737146e9e40944a60b
Asia Kane <asialkane@gmail.com>
2017-03-31 11:21:53 -0700

    add business logic to create a new array with any number divisible by three replaced with ping.

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,29 @@
 var posInteger = function(number){
   if (number <= 0){
     return "Invalid input.  Please enter an integer greater than 0."
-  } 
+  }
 };
 
 //Creates an array from 1 to the user input number.  Each element in array is a number.
 var createArray = function(number) {
-  debugger;
   var startArray = [];
   for (var i=1; i<=number; i++){
     startArray.push(i);
   }
   return startArray;
+};
+
+//Create new array with all values divisible by 3 replaced with "ping"
+var ping = function(array){
+  var changedArray = [];
+  array.forEach(function(number){
+    if(number % 3 === 0){
+      changedArray.push("ping");
+    } else {
+      changedArray.push(number);
+    }
+  });
+  return changedArray;
 };
 
 //User Interface Logic
@@ -22,7 +34,8 @@ $(document).ready(function(){
     event.preventDefault();
     let userInput = $("input#ppinput").val();
 
-    $("#ppoutput").text(createArray(userInput));
+    $("#ppoutput").text(ping(createArray(userInput)));
+    console.log(createArray(userInput));
     $("#ppoutput").text(posInteger(userInput));
   }); //Submit form close
 }); //Document.ready function close
